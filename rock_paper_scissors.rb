ada3b71a92bb1ec4dde8a874e242e40f493548e8
Martina Koleva <maartinakoleva@gmail.com>
2014-06-09 20:27:35 +0200

    User move Paper

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,9 +1,22 @@
 puts "Hello gamers!"
 puts "Choose your weapon!"
-#read user choice
-user_move = gets.chomp
-#define choices
+
+#user_move = gets.chomp
+user_move = "Paper"
+
 possible_moves = ["Rock", "Paper", "Scissors"]
-#random pc answer
+
 computer_move = possible_moves.sample 
 puts computer_move
+
+if user_move == "Paper"
+  if computer_move == "Scissors"
+    puts "You lose!"
+  end
+  if computer_move == "Paper"
+    puts "It is a tie!"
+  end
+  if computer_move == "Rock"
+    puts "You win!"
+  end
+end
