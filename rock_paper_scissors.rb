b0334f039acc84dc76ddba5dfa41f3e679372474
Martina Koleva <maartinakoleva@gmail.com>
2014-06-09 20:37:45 +0200

    Rules of game

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -10,13 +10,37 @@ computer_move = possible_moves.sample
 puts computer_move
 
 if user_move == "Paper"
+  if computer_move == "Rock"
+    puts "You win!"
+  end
+  if computer_move == "Paper"
+    puts "It's a tie!"
+  end
   if computer_move == "Scissors"
     puts "You lose!"
   end
+end
+
+if user_move == "Scissors"
   if computer_move == "Paper"
-    puts "It is a tie!"
+    puts "You win!"
+  end
+  if computer_move == "Scissors"
+    puts "It's a tie!"
   end
   if computer_move == "Rock"
+    puts "You lose!"
+  end
+end
+
+if user_move == "Rock"
+  if computer_move == "Scissors"
     puts "You win!"
+  end
+  if computer_move == "Rock"
+    puts "It's a tie!"
+  end
+  if computer_move == "Paper"
+    puts "You lose!"
   end
 end
