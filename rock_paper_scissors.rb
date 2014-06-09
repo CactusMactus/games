f40bff48dfede1531e408e4110ee3d13d849f341
Martina Koleva <maartinakoleva@gmail.com>
2014-06-09 19:59:54 +0200

    Ask user for a move and roll the dice

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,1 +1,9 @@
 puts "Hello gamers!"
+puts "Choose your weapon!"
+#choice=gets.chomp
+#puts choice
+choices=["Rock", "Paper", "Scissors"]
+#puts choices[1]
+#puts choices[rand(choices.length)]
+pc_choice=choices.sample 
+puts pc_choice
